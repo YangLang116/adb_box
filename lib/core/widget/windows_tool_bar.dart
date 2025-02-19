1210ce188f7c394193f64956edbdb93b4cd6df00
YangLang <yanglang116@gmail.com>
2025-02-19 23:44:36 +0800

    更新macos配置

--- lib/core/widget/windows_tool_bar.dart
+++ lib/core/widget/windows_tool_bar.dart
@@ -1,22 +1,13 @@
 import 'package:adb_box/core/constant/color_constant.dart';
 import 'package:adb_box/core/widget/window_buttons_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:window_manager/window_manager.dart';
 
-class WindowBar extends StatelessWidget implements PreferredSizeWidget {
-  final bool showBtn;
-  final bool showBack;
+class WindowsToolBar extends StatelessWidget implements PreferredSizeWidget {
   final Widget? lead;
   final Widget? action;
 
-  WindowBar({
-    super.key,
-    this.showBtn = true,
-    this.showBack = false,
-    this.lead,
-    this.action,
-  });
+  WindowsToolBar({super.key, this.lead, this.action});
 
   @override
   Widget build(BuildContext context) {
@@ -27,25 +18,13 @@ class WindowBar extends StatelessWidget implements PreferredSizeWidget {
         child: DragToMoveArea(
           child: Row(
             children: [
-              if (showBack) buildBackBtn(),
               if (lead != null) lead!,
               Spacer(),
               if (action != null) action!,
-              if (showBtn) WindowButtons(),
+              WindowButtons(),
             ],
           ),
         ),
-      ),
-    );
-  }
-
-  Widget buildBackBtn() {
-    return GestureDetector(
-      onTap: Get.back,
-      child: Icon(
-        Icons.arrow_back_rounded,
-        size: 25,
-        color: Colors.white,
       ),
     );
   }
